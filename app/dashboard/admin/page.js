359519b053f2d15f3253b5ae1a53b9e780742aca
NavedMemon <ljnaved01@gmail.com>
2025-07-02 15:42:46 +0530

    faltu

--- app/dashboard/admin/page.js
+++ app/dashboard/admin/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function page() {
-  return <div>adminnnnnnnnnnnnnnn</div>;
+  return <div>sanjil Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti placeat labore pariatur odit atque culpa et expedita, quae alias vel nisi eligendi iure, voluptatem delectus voluptas ex voluptate. Eius, eum!</div>;
 }
 
 export default page;
